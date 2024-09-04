bd8a95578ca86c78ad277ea67ef7a2bdbd3f84bd
Samhey-debug <142760185+Samhey-debug@users.noreply.github.com>
2024-09-04 20:28:38 +0100

    Update cc.js

--- api/cc.js
+++ api/cc.js
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
     try {
         // Fetch channels from the source guild
         const sourceChannelsResponse = await fetchWithRetry(`https://discord.com/api/v10/guilds/${sourceGuildId}/channels`, {
-            headers: { 'Authorization': process.env.ATCF }
+            headers: { 'Authorization': `Bot ${token}` }
         });
         const sourceChannels = await sourceChannelsResponse.json();
 
