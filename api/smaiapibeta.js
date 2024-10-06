f1d684f2e81e78e30bd2e3f44c5010a81710d6ef
Samhey-debug <142760185+Samhey-debug@users.noreply.github.com>
2024-10-06 13:52:08 +0100

    Update smaiapibeta.js

--- api/smaiapibeta.js
+++ api/smaiapibeta.js
@@ -718,10 +718,7 @@ const server = http.createServer((req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(serverConfig, null, 2));
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
+  } // here
 });
 
 const PORT = process.env.PORT || 3000;
